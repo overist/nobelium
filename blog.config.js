54c55dbd4a1c9b9c0807278cd992a4ef7a5a460e
overist <wj@over.ist>
2023-11-26 09:23:00 +0900

    chore: add googleSiteVerification

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   socialLink: '',
   seo: {
     keywords: ['overist', 'blog', 'notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: 'bxLa0OrllyPM4y3mGJLJz43DR9Zhb82gRuRuGfl9omY' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
